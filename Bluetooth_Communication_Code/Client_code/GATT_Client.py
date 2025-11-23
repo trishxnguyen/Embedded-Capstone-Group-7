209d437d9a9f96117fde8120b434e7b7632f07cc
Tim092025 <Tim092025@outlook.com>
2025-11-23 15:36:39 -0600

    Added bluetooth client and server code

---
+++ Bluetooth_Communication_Code/Client_code/GATT_Client.py
@@ -0,0 +1,29 @@
+import asyncio
+from bleak import BleakScanner
+from bleak import BleakClient
+#Pico address:
+addr = "2C:CF:67:E0:8A:CC"
+Characteristic = "3727f058-69dd-4d78-9b6f-9c2ebbb67d44"
+async def main(address):
+    # devices = await BleakScanner.discover()
+    # for d in devices:
+        # print(d)
+
+    async with BleakClient(address) as client:
+        print(type(client))
+        services = client.services.services.values()
+        for item in services:
+            print(f"Item: {item.characteristics}")
+
+            for characteristic in item.characteristics:
+                print(f"Characteristic: {characteristic}")
+                char_description = characteristic.description
+                char_uuid = characteristic.uuid
+                print(f"Characteristic Description is: {char_description}")
+                print(f"Characteristic UUID is: {char_uuid}")
+
+                value = await client.read_gatt_char(char_uuid)
+                print(f"Value: {value}")
+        print(services)
+       
+asyncio.run(main(addr))
