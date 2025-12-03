8cf846bba6bc9f754c0f3db940380bfb12bdbcbb
Tim092025 <Tim092025@outlook.com>
2025-12-03 09:42:58 -0600

    Fixing bluetooth writing

--- RTOS_CLASSROOM/RTOS_CLASSROOM.c
+++ RTOS_CLASSROOM/RTOS_CLASSROOM.c
@@ -26,42 +26,63 @@
 
 static uint16_t att_read_callback(hci_con_handle_t con_handle, uint16_t attribute_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size)
 {
-    UNUSED(con_handle);
-    //Current callback
-    printf("Read Callback");
+    // UNUSED(con_handle);
+    printf("Attribute Handle: %x\n",attribute_handle);
+    printf("Con Handle: %x\n",con_handle);
+    printf("Offset: %x\n",offset);
+    printf("Current Handle: %x\n",service_object.characteristic_current_handle);
+    printf("Fan Handle: %x\n",service_object.characteristic_fan_ctrl_handle);
+    printf("Light Handle: %x\n",service_object.characteristic_lights_ctrl_handle);
+
     //CURRENT
     if(attribute_handle == service_object.characteristic_current_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_blob(service_object.characteristic_current_value, strlen(service_object.characteristic_current_value), offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_current_configuration_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_little_endian_16(service_object.characteristic_current_client_configuration, offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_current_user_description_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_blob(service_object.characteristic_current_user_description,strlen(service_object.characteristic_current_user_description), offset, buffer, buffer_size);
     }
     //FAN CONTROL
     if(attribute_handle == service_object.characteristic_fan_ctrl_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_blob(service_object.characteristic_fan_ctrl_value, strlen(service_object.characteristic_fan_ctrl_value), offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_fan_ctrl_configuration_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_little_endian_16(service_object.characteristic_fan_ctrl_client_configuration, offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_fan_ctrl_user_description_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_blob(service_object.characteristic_fan_ctrl_user_description,strlen(service_object.characteristic_fan_ctrl_user_description), offset, buffer, buffer_size);
     }
     // LIGHT CONTROL
     if(attribute_handle == service_object.characteristic_lights_ctrl_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_blob(service_object.characteristic_lights_ctrl_value, strlen(service_object.characteristic_lights_ctrl_value), offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_lights_ctrl_configuration_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_little_endian_16(service_object.characteristic_lights_ctrl_client_configuration, offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_lights_ctrl_user_description_handle)
@@ -72,6 +93,8 @@ static uint16_t att_read_callback(hci_con_handle_t con_handle, uint16_t attribut
     // SCHEDULE
     if(attribute_handle == service_object.characteristic_schedule_handle)
     {
+    printf("READING-------------------------------\n");
+
         return att_read_callback_handle_blob(service_object.characteristic_schedule_value, strlen(service_object.characteristic_schedule_value), offset, buffer, buffer_size);
     }
     if(attribute_handle == service_object.characteristic_schedule_configuration_handle)
@@ -89,17 +112,35 @@ static uint16_t att_read_callback(hci_con_handle_t con_handle, uint16_t attribut
 
 static int att_write_callback(hci_con_handle_t con_handle, uint16_t attribute_handle, uint16_t transaction_mode, uint16_t offset, uint8_t * buffer, uint16_t buffer_size)
 {
-    UNUSED(transaction_mode);
-    UNUSED(offset);
-    UNUSED(buffer_size);
+    // printf("%s",attribute_handle);
+    // UNUSED(transaction_mode);
+    // UNUSED(offset);
+    // UNUSED(buffer_size);
+    // printf("WRITING-------------------------------\n");
+
+        //Current callback
+    printf("Attribute Handle: %x\n",attribute_handle);
+    printf("Con Handle: %x\n",con_handle);
+    printf("Transaction Mode: %x\n",transaction_mode);
+    printf("Offset: %x\n",offset);
+
+
+    printf("Current Handle: %x\n",service_object.characteristic_current_handle);
+
+    printf("Fan Handle: %x\n",service_object.characteristic_fan_ctrl_handle);
+
+    printf("Light Handle: %x\n",service_object.characteristic_lights_ctrl_handle);
+
     //If current service config is set to NOTIFY
     if(attribute_handle == ATT_CHARACTERISTIC_3727f058_69dd_4d78_9b6f_9c2ebbb67d44_01_VALUE_HANDLE)
     {
+
         service_object.characteristic_current_client_configuration = little_endian_read_16(buffer,0);
         service_object.con_handle = con_handle;
     }
     if(attribute_handle == service_object.characteristic_current_handle)
     {
+
         custom_service * instance = &service_object;
         buffer[buffer_size] = 0;
         memset(service_object.characteristic_current_value, 0, strlen(service_object.characteristic_current_value));
@@ -111,12 +152,52 @@ static int att_write_callback(hci_con_handle_t con_handle, uint16_t attribute_ha
             instance->callback_current.context = (void*) instance;
             att_server_register_can_send_now_callback(&instance->callback_current, instance->con_handle);
         }
+    }
+    if(attribute_handle== ATT_CHARACTERISTIC_74d31fe2_71d3_41c5_aa94_f15f9f61f3c7_01_VALUE_HANDLE)
+    {
+        printf("WRITING-------------------------------\n");
+
+        custom_service * instance = &service_object;
+        buffer[buffer_size] = 0;
+        printf("Inside fan handler\n");
+        memset(service_object.characteristic_fan_ctrl_value, 0, strlen(service_object.characteristic_fan_ctrl_value));
+        memcpy(service_object.characteristic_fan_ctrl_value, buffer, strlen(buffer));
+
+        if(instance->characteristic_fan_ctrl_configuration_handle)
+        {
+            instance->callback_fan_ctrl.callback = &characteristic_fan_ctrl_callback;
+            instance->callback_fan_ctrl.context = (void*) instance;
+            att_server_register_can_send_now_callback(&instance->callback_fan_ctrl, instance->con_handle);
+        }
+    
+
+        //Alert application of bluetooth rx
+
+    }
+    if(attribute_handle== service_object.characteristic_lights_ctrl_handle)
+    {
+        printf("Inside fan handler\n");
+
+        custom_service * instance = &service_object;
+        buffer[buffer_size] = 0;
+        memset(service_object.characteristic_lights_ctrl_value, 0, strlen(service_object.characteristic_lights_ctrl_value));
+        memcpy(service_object.characteristic_lights_ctrl_value, buffer, strlen(buffer));
+
+        if(instance->characteristic_lights_ctrl_configuration_handle)
+        {
+            instance->callback_lights_ctrl.callback = &characteristic_lights_ctrl_callback;
+            instance->callback_lights_ctrl.context = (void*) instance;
+            att_server_register_can_send_now_callback(&instance->callback_lights_ctrl, instance->con_handle);
+        }
+    
 
         //Alert application of bluetooth rx
 
     }
     
-
+        
+       
+    return 0;
 }
 
 
@@ -272,38 +353,30 @@ int BLE_StateSM(int state) {
 //END BLUETOOTH RELATED CODE
 
 //------TIMER RELATED CODE
-volatile bool timer_fired = false;
-char led_on = 0;
-int64_t alarm_callback(alarm_id_t id, __unused void *user_data) {
-    printf("Timer %d fired!\n", (int) id);
-    timer_fired = true;
-    led_on = ~led_on;
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-
-    // Can return a value here in us to fire in the future
-    return 0;
-}
 short test_cnt = 0;
+char led_on = 0;
+
 bool repeating_timer_callback(__unused struct repeating_timer *t) {
     // printf("Repeat at %lld\n", time_us_64());
     test_cnt++;
     if(test_cnt % 1000 == 0)
     {
         led_on = ~led_on;
-        printf("Led On: %d\n",led_on);
-        set_characteristic_current(led_on);
+
+        // set_characteristic_current(led_on);
 
         // printf("\n");
         // printf("One second \'Samples\': %d\n",test_cnt);
-        printf("Current Value: %d\n",*service_object.characteristic_current_value);
+        // printf("Current Value: %d\n",*service_object.characteristic_current_value);
         // printf("Fan Control: %s\n",service_object.characteristic_fan_ctrl_value);
         // printf("Light Control: %s\n",service_object.characteristic_lights_ctrl_value);
         // printf("Schedule: %s\n",service_object.characteristic_schedule_value);
         // printf("\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
 
     }
-    // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
     async_context_poll(cyw43_arch_async_context());
+    // async_context_wait_for_work_until(cyw43_arch_async_context(), at_the_end_of_time);
 
     return true;
 }
@@ -374,8 +447,8 @@ int main()
       // turn on bluetooth!
     hci_power_control(HCI_POWER_ON);
     set_characteristic_current(5);
-    set_characteristic_fan_ctrl(1);
-    set_characteristic_lights_ctrl(1);
+    set_characteristic_fan_ctrl(0);
+    set_characteristic_lights_ctrl(0);
     set_characteristic_schedule(100);
 
     // ----END BLUETOOTH RELATED INITILIZATION CODE ---
@@ -397,6 +470,5 @@ int main()
         // sleep_ms(100);
         // PACKET_HANDLER_FLAG = ~PACKET_HANDLER_FLAG;
         // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, PACKET_HANDLER_FLAG);
-        // async_context_wait_for_work_until(cyw43_arch_async_context(), at_the_end_of_time);
     }
 }
